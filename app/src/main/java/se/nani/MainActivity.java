fc9432ffa0a86c94e3586d8356301a039a89a32d
Nicole <nicole.silfverling@gmail.com>
2022-08-31 15:41:47 +0200

    clickable button

--- app/src/main/java/se/nani/MainActivity.java
+++ app/src/main/java/se/nani/MainActivity.java
@@ -1,5 +1,30 @@
 package se.nani;
 
-public class MainActivity {
-}
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
 
+public class MainActivity extends AppCompatActivity {
+    private int mCounter = 0;
+    ImageView btn;
+    TextView txv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = (ImageView) findViewById(R.id.button1);
+        txv = (TextView)findViewById(R.id.tx);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCounter ++;
+                txv.setText(Integer.toString(mCounter));
+            }
+        });
+    }
+}
