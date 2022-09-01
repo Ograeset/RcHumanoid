659e63d73fad5a66fe23fd5bf1c8759eb93fc03a
Ograeset <tedros963@gmail.com>
2022-09-01 12:08:54 +0200

    connected sound and renamed pngs

--- app/src/main/java/se/nani/MainActivity.java
+++ app/src/main/java/se/nani/MainActivity.java
@@ -1,5 +1,7 @@
 package se.nani;
 
+
+import android.media.MediaPlayer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -11,10 +13,27 @@ public class MainActivity extends AppCompatActivity {
     private int mCounter = 0;
     ImageView btn;
     TextView txv;
+    MediaPlayer mediaPlayer;
+    Button play;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.bend);
+
+        play = (Button)findViewById(R.id.play);
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.start();
+            }
+        });
+
+
+
 
         btn = (ImageView) findViewById(R.id.button1);
         txv = (TextView)findViewById(R.id.tx);
