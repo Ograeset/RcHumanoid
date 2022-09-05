651333aba892061e5224bf634f0ebc540828e007
Ograeset <tedros963@gmail.com>
2022-09-05 10:56:48 +0200

    background and invisible parts

--- app/src/main/java/se/nani/MainActivity.java
+++ app/src/main/java/se/nani/MainActivity.java
@@ -5,7 +5,7 @@ import android.media.MediaPlayer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
