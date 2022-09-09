280f0106cf06cfd21f03c3f3ed7f85121cda8d88
Nicole <nicole.silfverling@gmail.com>
2022-09-09 13:54:42 +0200

    buttons java

--- app/src/main/java/se/nani/MainActivity.java
+++ app/src/main/java/se/nani/MainActivity.java
@@ -16,7 +16,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
     MediaPlayer mediaPlayer;
-    Button arm, left, right, btnHead;
+    Button  btnCol1_R1, btnCol1_R2, btnCol1_R3, btnCol1_R4,
+            btnCol2_R1, btnCol2_R2, btnCol2_R3, btnCol2_R4,
+            btnCol3_R1, btnCol3_R2, btnCol3_R3, btnCol3_R4,
+            btnCol4_R1, btnCol4_R2, btnCol4_R3, btnCol4_R4,
+            btnCol5_R1, btnCol5_R2, btnCol5_R3, btnCol5_R4,
+            btnCol6_R1, btnCol6_R2, btnCol6_R3, btnCol6_R4;
     ImageView head;
     ImageView lArm;
     ImageView rArm;
@@ -28,19 +33,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
 
+        // Buttons
+        btnCol4_R2 = findViewById(R.id.btnCol4_R2);
+        btnCol6_R2 = findViewById(R.id.btnCol6_R2);
+        btnCol1_R2 = findViewById(R.id.btnCol1_R2);
+        btnCol2_R1 = findViewById(R.id.btnCol2_R1);
 
-        left = (Button)findViewById(R.id.btnCol4_R2);
-        right= (Button)findViewById(R.id.btnCol6_R2);
-        arm = (Button)findViewById(R.id.btnCol1_R2);
-        btnHead = (Button)findViewById(R.id.btnCol2_R1);
-        lArm = (ImageView)findViewById(R.id.arm_l);
-        rArm = (ImageView)findViewById(R.id.arm_r);
-        head = (ImageView)findViewById(R.id.head);
+        //Images
+        lArm = findViewById(R.id.arm_l);
+        rArm = findViewById(R.id.arm_r);
+        head = findViewById(R.id.head);
 
-        left.setOnClickListener(this);
-        right.setOnClickListener(this);
-        arm.setOnClickListener(this);
-        btnHead.setOnClickListener(this);
+        btnCol2_R1.setOnClickListener(this);
+        btnCol1_R2.setOnClickListener(this);
+        btnCol6_R2.setOnClickListener(this);
+        btnCol4_R2.setOnClickListener(this);
 
 
     }
@@ -54,20 +61,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         if(id == R.id.btnCol2_R1){ // head
             showImage(head);
+            mediaPlayer = MediaPlayer.create(this, R.raw.head);
 
         }
         else if (id == R.id.btnCol1_R2){ // right arm
             showImage(rArm);
             mediaPlayer = MediaPlayer.create(this, R.raw.arm);
-            mediaPlayer.start();
 
         }
+        mediaPlayer.start();
 
     }
 
-
-    public void playSound(){
-    }
 
     public void showImage(ImageView image) {
         Animation animFadeIn = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_in);
