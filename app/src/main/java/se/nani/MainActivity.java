dd640a35ee52dae70c4ee69270ef2f408783cfe6
Nicole <nicole.silfverling@gmail.com>
2022-09-13 14:55:34 +0200

    remove buttons

--- app/src/main/java/se/nani/MainActivity.java
+++ app/src/main/java/se/nani/MainActivity.java
@@ -179,17 +179,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnCol1_R1 = findViewById(R.id.btnCol1_R1);
         btnCol1_R2 = findViewById(R.id.btnCol1_R2);
         btnCol1_R3 = findViewById(R.id.btnCol1_R3);
-        btnCol1_R4 = findViewById(R.id.btnCol1_R4);
 
         btnCol2_R1 = findViewById(R.id.btnCol2_R1);
         btnCol2_R2 = findViewById(R.id.btnCol2_R2);
         btnCol2_R3 = findViewById(R.id.btnCol2_R3);
-        btnCol2_R4 = findViewById(R.id.btnCol2_R4);
 
         btnCol3_R1 = findViewById(R.id.btnCol3_R1);
         btnCol3_R2 = findViewById(R.id.btnCol3_R2);
         btnCol3_R3 = findViewById(R.id.btnCol3_R3);
-        btnCol3_R4 = findViewById(R.id.btnCol3_R4);
 
         btnCol4_R1 = findViewById(R.id.btnCol4_R1);
         btnCol4_R2 = findViewById(R.id.btnCol4_R2);
@@ -209,17 +206,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnCol1_R1.setOnClickListener(this);
         btnCol1_R2.setOnClickListener(this);
         btnCol1_R3.setOnClickListener(this);
-        btnCol1_R4.setOnClickListener(this);
 
         btnCol2_R1.setOnClickListener(this);
         btnCol2_R2.setOnClickListener(this);
         btnCol2_R3.setOnClickListener(this);
-        btnCol2_R4.setOnClickListener(this);
 
         btnCol3_R1.setOnClickListener(this);
         btnCol3_R2.setOnClickListener(this);
         btnCol3_R3.setOnClickListener(this);
-        btnCol3_R4.setOnClickListener(this);
 
         btnCol4_R1.setOnClickListener(this);
         btnCol4_R2.setOnClickListener(this);
